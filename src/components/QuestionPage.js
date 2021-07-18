67ee1e111da69100010f6550ba3eab4b1700e147
fitgeek96 <fitgeek96@gmail.com>
2021-07-18 14:37:21 +0300

    Complete QuestionPage

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,11 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-
+import UnansweredQuestion from './UnansweredQuestion';
+import AnsweredQuestion from './AnsweredQuestion';
 
 class QuestionPage extends Component {
     render() {
-        return (
+        const { authedUserAnswers, match } = this.props;
+        const id = match.params.id;
+        const answered = authedUserAnswers.hasOwnProperty(id) ? true : false;
 
+        return (
+            <Fragment>
+                <h2 className="text-center my-3">
+                    <small>Would You Rather ... ?</small>
+                </h2>
+                {answered ? <AnsweredQuestion id={id} /> : <UnansweredQuestion id={id} /> }
+            </Fragment>
         );
     }
 }
