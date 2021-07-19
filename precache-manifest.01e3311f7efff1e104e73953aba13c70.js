bf37846d99488daaed8dc432c2f8a13cf071ca69
fitgeek96 <fitgeek96@gmail.com>
2021-07-19 14:07:24 +0300

    Updates

---
+++ precache-manifest.01e3311f7efff1e104e73953aba13c70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c57246ea61aad941c728384d21eafa8",
+    "url": "/would_you_rather/index.html"
+  },
+  {
+    "revision": "6826aeb0bf5bfb861cbf",
+    "url": "/would_you_rather/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "87686717627541ef9616",
+    "url": "/would_you_rather/static/css/main.91c672c4.chunk.css"
+  },
+  {
+    "revision": "6826aeb0bf5bfb861cbf",
+    "url": "/would_you_rather/static/js/2.3e0e1439.chunk.js"
+  },
+  {
+    "revision": "87686717627541ef9616",
+    "url": "/would_you_rather/static/js/main.e594ff8a.chunk.js"
+  },
+  {
+    "revision": "b54f84c4d3df9e3e0a60",
+    "url": "/would_you_rather/static/js/runtime~main.5ca3baad.js"
+  }
+]);
